1549a659853c03f9e196fc1ebad8e34b468b82fd
night.dev <night.dev@hotmail.com>
2025-09-18 14:55:50 -0500

    latestreadying fix

--- routes/index.js
+++ routes/index.js
@@ -53,11 +53,11 @@ router.get("/", async (req, res) => {
      Status = latestReading.length && latestReading[0].HUMIDITY > "55" && latestReading[0].TEMP > "70" ? "alert" : "normal"
      if (Status === "alert") {
       if (alerts.length === 0 ){
-        alerts.push(lastReading.id)
+        alerts.push(latestReading.id)
         console.log(alerts)
       } else {
         if (alerts.length > 0) {
-          alerts.reduce(lastReading.id)
+          alerts.reduce(latestReading.id)
         }
       }
      }
